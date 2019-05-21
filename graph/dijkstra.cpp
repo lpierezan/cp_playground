eef78c7f0385146953e19aafed8adc2fb1a446d3
lpierezan <lucas.pierezan@gmail.com>
2019-05-21 18:20:53 -0300

    dijsktra c++ tested

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -10,12 +10,10 @@ typedef long long ll;
 
 class dijkstra{
 public:
-    
-    static vi solve(vector<vector<pii>> g, int s){
+    static vi solve(vector<vector<pii>> &g, int s, int target = -1){
         //g[v] = list of (u,w) , where v->u with weight w.
         int n = g.size();
         assert(s >= 0 && s < n);
-        priority_queue<int> SS;
 
         vi dist(n, -1);
         vector<bool> used(n, false);
@@ -28,9 +26,11 @@ public:
 
             int min_dist = min_data.first;
             int v = min_data.second;
-
             dist[v] = min_dist;
             used[v] = true;
+
+            if(target == v)
+                break;
 
             for(auto u_data : g[v]){
                 int u = u_data.first;
@@ -52,14 +52,36 @@ public:
     }
 };
 
-
 int main(){
-    //TODO test
-    
+    // https://www.spoj.com/problems/HIGHWAYS/
+
     int nt, it;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     cin >> nt;
     for(it=1;it<=nt;it++){
-        cout << "Case #" << it << ": ";
+        int n, m, s, t;
+        cin >> n >> m >> s >> t;
+        s--;t--;
+        
+        // build graph bidirectional from edges
+        vector<vector<pii>> g(n);
+        for(int i=0;i<m;i++){
+            int v,u,w;
+            cin >> v >> u >> w;
+            v--;u--;
+            g[v].push_back(mp(u,w));
+            g[u].push_back(mp(v,w));
+        }
+
+        vi dist;
+        dist = dijkstra::solve(g, s, t);
+        if(dist[t] < 0){
+            cout << "NONE" << endl;
+        }else{
+            cout << dist[t] << endl;
+        }
     }
     return 0;
 }
