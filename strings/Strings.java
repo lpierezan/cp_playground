9c842837d0cec00883347eb393a005bfcce5419c
Lucas Pierezan Magalhães <lucas.pierezan@gmail.com>
2019-08-15 00:36:41 -0300

    String to char[]

--- strings/Strings.java
+++ strings/Strings.java
@@ -1,37 +1,38 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Strings{
 
-    static int[] prefixFunction(String p){
-        int n = p.length();
+    static int[] prefixFunction(char[] p){
+        int n = p.length;
         int pi[] = new int[n];
         pi[0] = 0;
         for(int i=1; i<n; i++){
             int j = pi[i-1];
-            while(j > 0 && p.charAt(i) != p.charAt(j)){
+            while(j > 0 && p[i] != p[j]){
                 j = pi[j-1];
             }
-            if(p.charAt(i) == p.charAt(j))
+            if(p[i] == p[j])
                 pi[i] = j+1;
         }
         return pi;
     }
 
-    static int[] KMP(String p, String t){
+    static int[] KMP(char[] p, char[] t){
         // search p in l and returns list of initial hit positions
         List<Integer> hitL = new ArrayList<>();
         int pi[] = prefixFunction(p);
         int j = 0; // position in p to test
-        int m = p.length();
-        for(int i=0; i+(m-1-j) < t.length(); i++){
-            while(j > 0 && t.charAt(i) != p.charAt(j)){
+        int m = p.length;
+        for(int i=0; i+(m-1-j) < t.length; i++){
+            while(j > 0 && t[i] != p[j]){
                 j = pi[j-1];
             }
-            if(t.charAt(i) == p.charAt(j)){
+            if(t[i] == p[j]){
                 j += 1;                
             }
-            if(j == p.length()){
+            if(j == p.length){
                 hitL.add(i - j + 1);
                 j = pi[j-1];
             }
@@ -39,20 +40,21 @@ class Strings{
         return hitL.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    static int[][] prefixAutomaton(String p){
+    static int[][] prefixAutomaton(char[] p){
         //dfa[i][c] = new state when in state = i and receive char = 'a' + c
         //state i represents p[0...i] and last state (#) represents match
-        p += '#';
-        int n = p.length();
+        // p += '#'
+        
+        int n = p.length;
         int nL = 'z' - 'a' + 1;
-        int dfa[][] = new int[n][nL];
+        int dfa[][] = new int[n+1][nL];
         int pi[] = prefixFunction(p);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n+1;i++){
             for(int c=0;c<nL;c++){
-                if(i > 0 && c+'a' != p.charAt(i)){
+                if(i > 0 && (i == n || (c+'a' != p[i]))){
                     dfa[i][c] = dfa[pi[i-1]][c];
                 }else{
-                    if(c+'a' == p.charAt(i))
+                    if(i < n && (c+'a' == p[i]))
                         dfa[i][c] = i + 1;
                     else
                         dfa[i][c] = 0; // i == 0 and not match
@@ -61,5 +63,4 @@ class Strings{
         }
         return dfa;
     }
-    
 }
