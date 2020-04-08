c8bc53cb88c21ced607373822d853d03d2f164a5
Lucas Pierezan Magalhães <lucas.pierezan@gmail.com>
2020-04-08 18:52:35 -0300

    freeze bipartite matching

--- graph/BipartiteGraph.cpp
+++ graph/BipartiteGraph.cpp
@@ -7,8 +7,7 @@ typedef vector<int> vi;
 typedef pair<int, int> pii;
 typedef long long ll;
 
-class BipartiteGraph
-{
+class BipartiteGraph{
     /* 
         Bipartide graph with n vertices on the left and m vertices on the right.
         Index with [0,n-1] on the left and [n,n+m-1] on the right.
@@ -19,32 +18,34 @@ public:
     vector<vector<int>> g;
     int n, m;
 
-    BipartiteGraph(int n, int m) : n(n), m(m)
-    {
-        for (int i = 0; i < n + m; i++)
-            g.push_back(vi());
+    BipartiteGraph(int n, int m) : n(n), m(m), g(n+m,vi()){
     }
 
-    void add_edge(int v, int u)
-    {
+    void add_edge(int v, int u){
         if(u < v) swap(u,v);
         assert(v >= 0 && v < n && u >= n && u < n+m);
-
         g[v].push_back(u);
         g[u].push_back(v);
     }
 
-    bool augment(int v, vector<bool> &used, vector<int> &match)
-    {
+    void remove_edge(int v, int u){
+        if(u < v) swap(u,v);
+        assert(v >= 0 && v < n && u >= n && u < n+m);
+        int i = 2;
+        while(i--){
+            auto pos = find(all(g[v]), u);
+            if(pos != g[v].end()) g[v].erase(pos);
+            swap(u,v);
+        }
+    }
+
+    bool augment(int v, vector<bool> &used, vector<int> &match){
         used[v] = true;
-        for (int u : g[v])
-        {
+        for (int u : g[v]){
             if (used[u]) continue;
-            
             used[u] = true;
             bool found_path = false;
-            if (match[u] < 0)
-            {
+            if (match[u] < 0){
                 found_path = true;
             }else if(!used[match[u]]){
                 int vv = match[u];
@@ -61,21 +62,34 @@ public:
         return false;
     }
 
-    int max_matching()
-    {
+    int max_matching(vi &match, const vector<pii> &freeze){
         vector<bool> used(n + m);
-        vector<int> match(n+m, -1);
-
+        match.resize(n+m);
+        fill(all(match), -1);
         int ans = 0;
+        for(auto x : freeze){
+            match[x.first] = x.second;
+            ans++;
+        }
         for (int i = 0; i < n; i++)
         {
             fill(all(used), false);
+            for(auto x : freeze){
+                used[x.first] = true;
+                used[x.second] = true;
+            }
+            if(used[i]) continue;
             if (augment(i, used, match))
                 ans++;
             if(ans == min(n,m))
                 break;
         }
         return ans;
+    }
+
+    int max_matching(){
+        vi _; vector<pii> __;
+        return max_matching(_, __);
     }
 
     string to_string(){
